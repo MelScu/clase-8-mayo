fb31070fc10f048eec40ee9a234db002a263a745
MelScu <164791032+MelScu@users.noreply.github.com>
2024-05-08 12:14:28 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("Hello world!")
+
+
+function modificartitulo() {
+    console.log("activandofuncionalidad")
+    let textoinput = document.querySelector("#textocaja").value;
+    let h2TituloPrimero = document.querySelector("#tituloprincipal")
+    console.log(textoinput)
+}
